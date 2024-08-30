b34357181a22fd6ccc6566d8e0ae71d5aedab4a3
webgeekhabib007 <hr665102@gmail.com>
2024-08-30 19:59:19 +0600

    models are set

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    title = models.CharField(max_length=200,name="title")
+    price = models.DecimalField(name="price",max_digits=10,decimal_places=2)
+    inventory = models.IntegerField(name='inventory')
+
+
+class Booking(models.Model):
+    name = models.CharField(max_length=200,name='name')
+    no_of_guest = models.IntegerField(name='no_of_guest')
+    booking_date = models.DateField(name='booking_date')
